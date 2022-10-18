4f63a07e9e9dda80b7c4f6c012f07b1fa2b7d686
Guilherme Braga gomes <gui@guibgomes.com>
2022-10-18 19:22:50 +0200

    Started dealing with signals

--- client.c
+++ client.c
@@ -4,12 +4,19 @@
 #include <signal.h>
 #include <sys/types.h>
 
-int main(void) {
-    int pid = 0;
+void handle_sigtstp(int sig) {
+    printf("Stop not allowed\n");
+}
 
+int main(void) {
+    struct sigaction sa;
+
+    sa.sa_handler = &handle_sigtstp;
+    sa.sa_flags = SA_RESTART;
+    sigaction(SIGTSTP, &sa, NULL);
+
+    int pid = 0;
     printf("Introduce the PID: ");
     scanf("%d", &pid);
-
-    kill(pid, SIGINT);
+    return 0;
 } 
-
